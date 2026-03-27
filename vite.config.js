91a3113dfe32eb7fd38cd8a07b488b8ba5c44e0a
TanushreeSoni2021 <tanushrees.bca2021@ssism.org>
2026-03-27 17:07:42 +0530

    base change

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react-swc'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "https://tanushreesoni2021.github.io/portfolio/index.html",
+  base: "/portfolio",
 })
